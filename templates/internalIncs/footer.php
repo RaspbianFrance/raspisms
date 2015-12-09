0a5cae859c7cc1a062ce92c0426cc0c5af409144
OsaAjani <pierre.lin@free.fr>
2015-12-09 20:15:22 +0100

    Ajout activation/desactivation son sur reception message

--- templates/internalIncs/footer.php
+++ templates/internalIncs/footer.php
@@ -2,10 +2,12 @@
 		RaspiSMS a été créé pour vous par le site <a href="http://raspbian-france.fr">Raspbian-France</a>, site dédié à la Raspberry Pi<br/>
 		Copyright 2014. RaspiSMS est un programme sous <a href="https://www.gnu.org/licenses/gpl.txt" rel="nofollow">licence GNU GPL</a>.<br/>
 	</footer>
-	<audio id="reception-sound">
-		<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.ogg" type="audio/ogg">
-		<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.mp3" type="audio/mpeg">
-		<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.wav" type="audio/wav">
-	</audio> 
+	<?php if (RASPISMS_SETTINGS_SMS_RECEPTION_SOUND) { ?>
+		<audio id="reception-sound">
+			<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.ogg" type="audio/ogg">
+			<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.mp3" type="audio/mpeg">
+			<source src="<?php echo HTTP_PWD; ?>sounds/receptionSound.wav" type="audio/wav">
+		</audio>
+	<?php } ?>
 	</body>
 </html>
