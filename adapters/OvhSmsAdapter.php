f609fc175f51d979433d12c8795d177d0f882280
osaajani <pierre.lin@free.fr>
2019-11-12 05:18:32 +0100

    add adapters

--- adapters/OvhSmsAdapter.php
+++ adapters/OvhSmsAdapter.php
@@ -8,42 +8,57 @@
      *
      * All Phone Adapters must implement this interface
      */
-    interface AdapterInterface
+    class OvhSmsAdapter implements AdapterInterface
     {
         /**
          * Classname of the adapter
          */
-        public static function meta_classname() : string;
+        public static function meta_classname() : string { return __CLASS__; }
 
         /**
          * Name of the adapter.
          * It should probably be the name of the service it adapt (e.g : Gammu SMSD, OVH SMS, SIM800L, etc.)
          */
-        public static function meta_name() : string;
+        public static function meta_name() : string { return 'OVH SMS'; }
 
         /**
          * Description of the adapter.
          * A short description of the service the adapter implements.
          */
-        public static function meta_description() : string;
+        public static function meta_description() : string { return 'Solution de SMS proposé par le groupe OVH, https://www.ovhtelecom.fr/sms/.'; }
         
         /**
          * Description of the datas expected by the adapter to help the user. (e.g : A list of expecteds Api credentials fields, with name and value)
          */
-        public static function meta_datas_help() : string;
+        public static function meta_datas_help() : string { return 'See later.'; } 
 
         /**
          * Does the implemented service support flash smss
          */
-        public static function meta_support_flash() : bool;
+        public static function meta_support_flash() : bool { return false ; }
 
 
+        /**
+         * Phone number using the adapter
+         */
+        private $number;
+
+        /**
+         * Datas used to configure interaction with the implemented service. (e.g : Api credentials, ports numbers, etc.).
+         */
+        private $datas;
+
+        
         /**
          * Adapter constructor, called when instanciated by RaspiSMS
          * @param string $number : Phone number the adapter is used for
          * @param json string $datas : JSON string of the datas to configure interaction with the implemented service
          */
-        public function __construct (string $number, string $datas);
+        public function __construct (string $number, string $datas)
+        {
+            $this->number = $number;
+            $this->datas = $datas;
+        }
     
     
         /**
@@ -53,7 +68,10 @@
          * @param bool $flash : Is the SMS a Flash SMS
          * @return bool : True if send, False else
          */
-        public function send (string $destination, string $text, boolean $flash) : boolean;
+        public function send (string $destination, string $text, boolean $flash) : boolean
+        {
+            return true;
+        }
 
 
         /**
@@ -61,5 +79,8 @@
          * @param float $since : Unix microtime representation of the date from wich we want to read the SMSs
          * @return array : Array of the sms reads
          */
-        public function read (float $since) : array;
+        public function read (float $since) : array
+        {
+            return [];
+        }
     }
---
+++ controllers/internals/Adapter.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * This file is part of RaspiSMS.
+ *
+ * (c) Pierre-Lin Bonnemaison <plebwebsas@gmail.com>
+ *
+ * This source file is subject to the GPL-3.0 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace controllers\internals;
+
+    /**
+     * Class to interact with adapters
+     */
+    class Adapter extends \descartes\InternalController
+    {
+        private const ADAPTERS_FILES_END = 'Adapter.php';
+        private const ADAPTERS_META_START = 'meta_';
+
+        /**
+         * List adapters using internal metas
+         * @return array
+         */
+        public function list_adapters ()
+        {
+            $adapters = [];
+
+            $files = $this->list_files();
+            if (!$files)
+            {
+                return $adapters;
+            }
+
+            foreach ($files as $file)
+            {
+                $metas = $this->read_adapter_metas($file);
+                if (!$metas)
+                {
+                    continue;
+                }
+
+                $adapters[] = $metas;
+            }
+
+            return $adapters;
+        }
+
+
+        /**
+        * List Adapters files
+        * @return mixed (false|array) : array of adapters files path
+        */
+        public function list_files ()
+        {
+            if (!is_readable(PWD_ADAPTERS))
+            {
+                return false;
+            }
+
+            $adapters_files = [];
+
+            $files = scandir(PWD_ADAPTERS);
+            foreach ($files as $filename)
+            {
+                $len = mb_strlen(self::ADAPTERS_FILES_END);
+                $end = mb_substr($filename, -$len);
+                if ($end !== self::ADAPTERS_FILES_END)
+                {
+                    continue;
+                }
+
+                $adapters_files[] = PWD_ADAPTERS . '/' . $filename;
+            }
+
+            return $adapters_files;
+        }
+
+        /**
+         * Read constants of an adapter
+         * @return mixed(array|bool) : False on error, array of constants name => value
+         */
+        public function read_adapter_metas ($adapter_file)
+        {
+            $metas = [];
+            
+            if (!is_readable($adapter_file))
+            {
+                return false;
+            }
+
+            $adapter_classname = pathinfo($adapter_file, PATHINFO_FILENAME);
+            $reflection_class = new \ReflectionClass('\adapters\\' . $adapter_classname);
+            if (!$reflection_class)
+            {
+                return false;
+            }
+
+            $methods = $reflection_class->getMethods(\ReflectionMethod::IS_STATIC);
+            foreach ($methods as $method)
+            {
+                $start_with = mb_substr($method->getName(), 0, mb_strlen(self::ADAPTERS_META_START));
+
+                if ($start_with !== self::ADAPTERS_META_START)
+                {
+                    continue;
+                }
+
+                $metas[$method->getName()] = $method->invoke(null);
+            }
+
+            return $metas;
+        }
+    }
