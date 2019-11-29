6503eb62ec18405022adaa58702d8007a6a8ee19
osaajani <pierre.lin@free.fr>
2019-11-29 06:02:13 +0100

    fix footer link

--- templates/incs/footer.php
+++ templates/incs/footer.php
@@ -1,5 +1,5 @@
 	<footer class="text-center">
-		RaspiSMS a été créé pour vous par le site <a href="http://raspbian-france.fr">Raspbian-France</a>, site dédié à la Raspberry Pi<br/>
+		RaspiSMS a été créé pour vous par le site <a href="https://raspberry-pi.fr">Raspberry Pi FR</a>, site dédié à la Raspberry Pi<br/>
 		Copyright 2014. RaspiSMS est un programme sous <a href="https://www.gnu.org/licenses/gpl.txt" rel="nofollow">licence GNU GPL</a>.<br/>
     </footer>
 
