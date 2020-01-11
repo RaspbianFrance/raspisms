a9b9d581be5e4fe7c655a7f295cf662627cfe306
osaajani <pierre.lin@free.fr>
2020-01-11 17:30:56 +0100

    fix typo resulting in error generating page reset password

--- templates/connect/forget-password.php
+++ templates/connect/forget-password.php
@@ -5,7 +5,7 @@
 ?>
 <div class="container-fluid">
 	<div class="row">
-		<form class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-4 connexion-form" action="<?php echo \descartes\Router::url('connect', 'send_reset_password', ['csrf' => $_SESSION['csrf']]); ?>" method="POST">
+		<form class="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-4 connexion-form" action="<?php echo \descartes\Router::url('Connect', 'send_reset_password', ['csrf' => $_SESSION['csrf']]); ?>" method="POST">
 			<h2>Mot de passe oublié</h2>
 			<div class="form-group">
 				<label>Adresse e-mail</label>
