38a8b023cb6b5c057cba5a930d756a5e099ccc49
osaajani <pierre.lin@free.fr>
2020-02-18 20:03:32 +0100

    remove makefile

--- raspisms/env.dev.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $env = [
-        //Database access
-        'DATABASE_HOST' => 'localhost',
-        'DATABASE_NAME' => 'raspisms',
-        'DATABASE_USER' => 'root',
-        'DATABASE_PASSWORD' => 'root',
-
-    ];
