99edf9a2c8d450d83e000201dbf32a666759fb26
osaajani <pierre.lin@free.fr>
2020-02-18 22:01:58 +0100

    Remove test function

--- controllers/internals/Console.php
+++ controllers/internals/Console.php
@@ -58,45 +58,4 @@ namespace controllers\internals;
 
             new \daemons\Phone($phone);
         }
-
-        /**
-         * Cette fonction retourne la fenetre de connexion.
-         *
-         * @param mixed $id_phone
-         */
-        public function test($id_phone)
-        {
-            $bdd = \descartes\Model::_connect(DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD, 'UTF8');
-            $internal_phone = new \controllers\internals\Phone($bdd);
-            $phone = $internal_phone->get($id_phone);
-            if (!$phone)
-            {
-                echo "No phone for id : {$id_phone}\n";
-
-                return false;
-            }
-
-            echo "Found phone for id : {$id_phone}\n";
-
-            $adapter_classname = $phone['adapter'];
-            $adapter = new $adapter_classname($phone['number'], $phone['adapter_datas']);
-
-            //Try send a message
-            /*
-            $destination = '+33669529042';
-            $text = "Coucou c'est pour un test !";
-            $flash = false;
-            $uid = $adapter->send($destination, $text, $flash);
-
-            if (!$uid)
-            {
-                echo "Cannot send message to $destination\n";
-                return false;
-            }
-
-            echo "Send a message to $destination with uid $uid \n";
-            */
-            $smss = $adapter->read();
-            var_dump($smss);
-        }
     }
