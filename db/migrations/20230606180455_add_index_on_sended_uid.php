347084b5c466d793d6fa19bbc7a95ea843e27233
osaajani <>
2023-06-06 20:32:04 +0200

    improve perfs on status update by sizing down uid and adding an index

---
+++ db/migrations/20230606180455_add_index_on_sended_uid.php
@@ -0,0 +1,24 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddIndexOnSendedUid extends AbstractMigration
+{
+    /**
+     * Modify sended uid and call to be 100 char long, we dont need a 500 char uid and too long a char ss hurting perfs
+     * Add index on sended uid to make status update more efficient
+     */
+    public function change()
+    {
+        $table = $this->table('sended');
+        $table->changeColumn('uid', 'string', ['limit' => 100]);
+        $table->addIndex('uid');
+        $table->update();
+
+
+        $table = $this->table('call');
+        $table->changeColumn('uid', 'string', ['limit' => 100]);
+        $table->addIndex('uid');
+        $table->update();
+    }
+}
